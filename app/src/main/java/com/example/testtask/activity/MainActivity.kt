dd87e12e029fbe0b21ead2385ece6528bbe99c01
Lapoushko <vadim.dovgiy@mail.ru>
2024-05-10 15:47:48 +0500

    Add pagination

--- app/src/main/java/com/example/testtask/activity/MainActivity.kt
+++ app/src/main/java/com/example/testtask/activity/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.testtask.activity
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.NestedScrollView
 import com.example.testtask.R
 import com.example.testtask.component.OnPictureClickListener
 import com.example.testtask.controller.PictureDataControllerImpl
@@ -11,10 +13,14 @@ import com.example.testtask.databinding.ActivityMainBinding
 import com.example.testtask.model.Picture
 import com.example.testtask.service.SerperServiceImpl
 
+
 class MainActivity : AppCompatActivity(), OnPictureClickListener {
     private lateinit var binding: ActivityMainBinding
     private lateinit var serperService: SerperServiceImpl
     private lateinit var pictureDataController: PictureDataControllerImpl
+
+    private var page = 1
+    private lateinit var query: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +30,17 @@ class MainActivity : AppCompatActivity(), OnPictureClickListener {
         initServicesAndControllers()
         setupListeners()
         setupWindowInsets()
+
+        binding.nestedScrollView.setOnScrollChangeListener(NestedScrollView.OnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
+            // on scroll change we are checking when users scroll as bottom.
+            if (scrollY == v.getChildAt(0).measuredHeight - v.measuredHeight) {
+                // in this method we are incrementing page number,
+                // making progress bar visible and calling get data method.
+                page++
+                serperService.searchImagesByQuery(query, page)
+                pictureDataController.updatePictures(serperService.getPictures())
+            }
+        })
     }
 
     /**
@@ -40,12 +57,15 @@ class MainActivity : AppCompatActivity(), OnPictureClickListener {
      */
     private fun setupListeners() {
         binding.searchButton.setOnClickListener {
-            val query = binding.search.text.toString()
-            serperService.searchImagesByQuery(query, 1)
+            query = binding.search.text.toString()
+            serperService.searchImagesByQuery(query, page)
             pictureDataController.updatePictures(serperService.getPictures())
         }
     }
 
+    /**
+     * Установка верхнего бара
+     */
     private fun setupWindowInsets() {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
