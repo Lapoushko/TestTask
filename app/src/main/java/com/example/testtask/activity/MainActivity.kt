5461525a397fc36708d96cd6130366e53075aa27
lapoushko <vadim.dovgiy@mail.ru>
2024-05-13 23:08:46 +0500

    Refactoring

--- app/src/main/java/com/example/testtask/activity/MainActivity.kt
+++ app/src/main/java/com/example/testtask/activity/MainActivity.kt
@@ -1,83 +1,94 @@
 package com.example.testtask.activity
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.widget.NestedScrollView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.testtask.R
 import com.example.testtask.component.OnPictureClickListener
 import com.example.testtask.controller.PictureDataControllerImpl
 import com.example.testtask.databinding.ActivityMainBinding
 import com.example.testtask.model.Picture
+import com.example.testtask.repository.PictureRepository
+import com.example.testtask.repository.PictureRepositoryImpl
 import com.example.testtask.service.SerperServiceImpl
+import okhttp3.OkHttpClient
 
-
+/**
+ * Основная активность
+ */
 class MainActivity : AppCompatActivity(), OnPictureClickListener {
     private lateinit var binding: ActivityMainBinding
     private lateinit var serperService: SerperServiceImpl
     private lateinit var pictureDataController: PictureDataControllerImpl
 
+    private val pictureRepository: PictureRepository = PictureRepositoryImpl()
     private var page = 1
-    private lateinit var query: String
+    private var query: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initServicesAndControllers()
+        initControllers()
         setupListeners()
-        setupWindowInsets()
 
-        binding.nestedScrollView.setOnScrollChangeListener(NestedScrollView.OnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
-            // on scroll change we are checking when users scroll as bottom.
-            if (scrollY == v.getChildAt(0).measuredHeight - v.measuredHeight) {
-                // in this method we are incrementing page number,
-                // making progress bar visible and calling get data method.
-                page++
-                serperService.searchImagesByQuery(query, page)
-                pictureDataController.updatePictures(serperService.getPictures())
-            }
-        })
+        scrolling()
     }
 
     /**
-     * Инициализация сервисов и контроллеров
+     * Инициализация контроллера
      */
-    private fun initServicesAndControllers() {
-        serperService = SerperServiceImpl()
-        pictureDataController = PictureDataControllerImpl(serperService, binding.recyclerView, this)
+    private fun initControllers() {
+        serperService = SerperServiceImpl(OkHttpClient(), pictureRepository)
+        pictureDataController = PictureDataControllerImpl(
+            binding.recyclerView,
+            this,
+            pictureRepository
+        )
         pictureDataController.setupRecyclerView()
     }
 
     /**
-     * Кнопка поиска
+     * Установка слушателя
      */
     private fun setupListeners() {
         binding.searchButton.setOnClickListener {
-            query = binding.search.text.toString()
+            if (binding.search.text.toString()!= query) {
+                pictureRepository.deletePictures()
+                query = binding.search.text.toString()
+            }
             serperService.searchImagesByQuery(query, page)
-            pictureDataController.updatePictures(serperService.getPictures())
+            pictureDataController.updatePictures(pictureRepository.getPictures())
         }
     }
 
-    /**
-     * Установка верхнего бара
-     */
-    private fun setupWindowInsets() {
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
     override fun onPictureClick(pictures: List<Picture>, pos: Int) {
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.picture_holder, PicturesFragment.newInstance(pictures, pos))
-                .addToBackStack(null)
-                .commit()
-        }
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.picture_holder, PicturesFragment.newInstance(pictures, pos))
+            .addToBackStack(null)
+            .commit()
+    }
+
+    /**
+     * Пагинация списка
+     */
+    private fun scrolling(){
+        binding.recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+                val totalItemCount = layoutManager.itemCount
+                val lastVisibleItemPosition = layoutManager.findLastVisibleItemPosition()
+
+                if (lastVisibleItemPosition + 10 >= totalItemCount) {
+                    page++
+                    serperService.searchImagesByQuery(query, page)
+                    pictureDataController.updatePictures(pictureRepository.getPictures())
+                }
+            }
+        })
+    }
 }
